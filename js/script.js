1320df5dc31a3ea44ede2148417c8497b5dd2757
Kzlyr Shaira Manejo <kmanejo@up.edu.ph>
2023-10-01 00:05:41 +0800

    Add elements and tidy up

--- js/script.js
+++ js/script.js
@@ -19,23 +19,40 @@ sr.reveal('.menu-text', {delay: 200, origin:'top'});
 sr.reveal('.menu-img', {delay: 450, origin:'top'});
 sr.reveal('.icons', {delay: 500, origin:'left'});
 sr.reveal('.infosection h2', {delay: 200, origin:'top'});
-sr.reveal('.info-card', {delay: 400, origin:'left'});
-sr.reveal('.slider-container h2', {delay: 200, origin:'top'});
-sr.reveal('.wrapper', {delay: 50, origin:'left'});
+sr.reveal('.infosection', {delay: 500, origin:'left'});
+sr.reveal('.slider-container h2', {delay: 100, origin:'top'});
+sr.reveal('.wrapper', {delay: 70, origin:'left'});
 
 
 /********change bg color on scroll*********/
-function changeBg(){
-	var header= document.getElementById('header');
-	var scrollValue = window.scrollY;
-	if(scrollValue < 110){
-		header.classList.remove('header');
-	}
-	else{
-		header.classList.add('headerOnScroll');
-	}
+function changeBg() {
+  var header = document.getElementById('header');
+  var scrollValue = window.scrollY;
+
+  if (scrollValue < 500) {
+    header.classList.remove('headerOnScroll');
+    header.classList.add('header');
+  } else {
+    header.classList.remove('header');
+    header.classList.add('headerOnScroll');
+  }
 }
 
 window.addEventListener('scroll', changeBg);
+
+
+/*************scroll down button***********/
+
+document.getElementById('scroll-link').addEventListener('click', function (event) {
+  event.preventDefault();
+
+  const targetId = this.getAttribute('href').substring(1);
+  const targetElement = document.getElementById(targetId);
+
+  window.scrollTo({
+    top: targetElement.offsetTop,
+    behavior: 'smooth'
+  });
+});
 
 
